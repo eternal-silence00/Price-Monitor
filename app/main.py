548baa2289887a7c1d752163ab07b7363058bb4a
eternal-silence00 <andron555666777@gmail.com>
2026-05-18 20:40:38 +0300

    feat: add main.py

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.database import engine
+from contextlib import asynccontextmanager
+from app.routers import auth, tracking, price_history
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    yield
+    await engine.dispose()
+    
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth.router)
+app.include_router(tracking.router)
+app.include_router(price_history.router)
